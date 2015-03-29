59dcc1d1bd1fad5fc867954ddc50302dbf67285b
Radek Slupik <rightfold@gmail.com>
2015-03-29 21:37:56 +0200

    Fix minor formatting issue

--- mill/src/main.cpp
+++ mill/src/main.cpp
@@ -18,11 +18,11 @@
 
 using namespace mill;
 
-int main(int argc, char const** argv) {
-    if (argc != 2) {
+int main(int argc, char const** argv) <%
+    if (argc != 2) <%
         std::cerr << "usage: mill hello.millo\n";
         return 1;
-    }
+    %>
 
     baka::io::unique_fd objectFD(open(argv[1], O_RDONLY));
     baka::io::file_stream<baka::io::unique_fd, baka::io::operation::read> objectReader(std::move(objectFD));
@@ -32,28 +32,28 @@ int main(int argc, char const** argv) {
     vm.loadObject(object);
 
     std::mutex coutMutex;
-    vm.setGlobal("std::io::writeln", make<Subroutine>([&] (VM&, std::size_t, Value** argv) -> boost::intrusive_ptr<Value> {
+    vm.setGlobal("std::io::writeln", make<Subroutine>([&] (VM&, std::size_t, Value** argv) -> boost::intrusive_ptr<Value> <%
         std::lock_guard<decltype(coutMutex)> lock(coutMutex);
         std::cout << dynamic_cast<String&>(*argv[0]).value << '\n';
         return &Unit::instance();
-    }));
-    vm.setGlobal("std::always::infix~", make<Subroutine>([&] (VM&, std::size_t, Value** argv) {
+    %>));
+    vm.setGlobal("std::always::infix~", make<Subroutine>([&] (VM&, std::size_t, Value** argv) <%
         auto& a = dynamic_cast<String&>(*argv[0]);
         auto& b = dynamic_cast<String&>(*argv[1]);
         return make<String>(a.value + b.value);
-    }));
+    %>));
 
     std::vector<std::future<boost::intrusive_ptr<Value>>> results;
-    for (auto i = 0; i < 100; ++i) {
+    for (auto i = 0; i < 100; ++i) <%
         results.push_back(vm.call(vm.global("main::MAIN").get(), 0, nullptr));
-    }
+    %>
     sleep(1);
-    for (auto i = 0; i < 100; ++i) {
+    for (auto i = 0; i < 100; ++i) <%
         results.push_back(vm.call(vm.global("main::MAIN").get(), 0, nullptr));
-    }
-    for (auto&& result : results) {
+    %>
+    for (auto&& result : results) <%
         result.get();
-    }
+    %>
 
     return 0;
-}
+%>
--- mill/src/object.hpp
+++ mill/src/object.hpp
@@ -9,58 +9,58 @@
 #include <utility>
 #include <vector>
 
-namespace mill {
+namespace mill <%
     BAKA_EXCEPTION(BadObject, std::runtime_error);
 
-    struct Object {
-        struct Subroutine {
+    struct Object <%
+        struct Subroutine <%
             std::size_t name;
             std::size_t parameterCount;
             std::vector<unsigned char> body;
-        };
+        %>;
 
         std::vector<std::string> strings;
         std::size_t name;
         std::vector<std::size_t> dependencies;
         std::vector<Subroutine> subroutines;
-    };
+    %>;
 
     template<typename Reader>
-    Object readObject(Reader& reader) {
+    Object readObject(Reader& reader) <%
 #define INT(T) baka::read_integer<T>(reader, baka::byte_order::little_endian)
 
         std::string magic(4, '\0');
         baka::io::read_full(reader, &magic[0], &magic[0] + magic.size());
-        if (magic != "\xDE\xAD\xBE\xEF") {
+        if (magic != "\xDE\xAD\xBE\xEF") <%
             throw BadObject("bad magic number");
-        }
+        %>
 
         auto versionMajor = INT(std::uint16_t);
         auto versionMinor = INT(std::uint16_t);
         auto versionPatch = INT(std::uint16_t);
-        if (versionMajor != 0 && versionMinor != 0 && versionPatch != 1) {
+        if (versionMajor != 0 && versionMinor != 0 && versionPatch != 1) <%
             throw BadObject("bad version number");
-        }
+        %>
 
         auto stringCount = INT(std::uint32_t);
         std::vector<std::string> strings(stringCount);
-        for (auto& string : strings) {
+        for (auto& string : strings) <%
             auto stringLength = INT(std::uint32_t);
             string.resize(stringLength);
             baka::io::read_full(reader, &string[0], &string[0] + string.size());
-        }
+        %>
 
         auto name = INT(std::uint32_t);
 
         auto dependencyCount = INT(std::uint32_t);
         std::vector<std::size_t> dependencies(dependencyCount);
-        for (auto& dependency : dependencies) {
+        for (auto& dependency : dependencies) <%
             dependency = INT(std::uint32_t);
-        }
+        %>
 
         auto subroutineCount = INT(std::uint32_t);
         std::vector<Object::Subroutine> subroutines(subroutineCount);
-        for (auto& subroutine : subroutines) {
+        for (auto& subroutine : subroutines) <%
             subroutine.name = INT(std::uint32_t);
             subroutine.parameterCount = INT(std::uint32_t);
             auto bodyLength = INT(std::uint32_t);
@@ -70,7 +70,7 @@ namespace mill {
                 (char*)subroutine.body.data(),
                 (char*)subroutine.body.data() + subroutine.body.size()
             );
-        }
+        %>
 
         Object object;
         object.strings = std::move(strings);
@@ -80,5 +80,5 @@ namespace mill {
         return object;
 
 #undef INT
-    }
-}
+    %>
+%>
