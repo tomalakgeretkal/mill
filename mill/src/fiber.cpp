7770dd660904edc423e0db75bfc21b845669e63b
Radek Slupik <rightfold@gmail.com>
2015-04-04 22:23:16 +0200

    Make current_fiber pointer volatile, not pointee

--- mill/src/fiber.cpp
+++ mill/src/fiber.cpp
@@ -3,7 +3,7 @@
 #include "fiber.hpp"
 
 namespace {
-    __thread mill::fiber volatile* current_fiber = nullptr;
+    __thread mill::fiber* volatile current_fiber = nullptr;
 }
 
 void mill::fiber::resume() {
