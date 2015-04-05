2c56d1aa632f2f43ba41b56d4a411809fa58a415
Radek Slupik <rightfold@gmail.com>
2015-04-05 15:34:34 +0200

    Document thread_pool

--- mill/src/thread_pool.hpp
+++ mill/src/thread_pool.hpp
@@ -5,14 +5,19 @@
 #include <cstddef>
 
 namespace mill {
+    // A pool of threads to which tasks can be submitted.
     class thread_pool {
     public:
+        // Construct the thread pool with an optimal number of threads.
         thread_pool();
 
+        // Construct the thread pool with a custom number of threads.
         explicit thread_pool(std::size_t threadCount);
 
+        // Wait until all tasks have finished and stop the thread pool.
         ~thread_pool();
 
+        // Enqueue a task.
         template<typename F>
         void post(F function);
 
